15904d7eed86f9739992b9b5eedd43e9ba047e12
ura <ura@itx.ru>
2021-05-19 13:26:57 +0300

    /

--- bundle/apjqph/defs.php
+++ bundle/apjqph/defs.php
@@ -1,5 +1,5 @@
 <?php
 define('CM_TOP',realpath(dirname(__FILE__).'/../..'));
 define('CM_ROOT',realpath(dirname(__FILE__)));
-define('CM_WHOME',$_SERVER['REQUEST_SCHEME'] .'://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']);
+define('CM_WHOME',$_SERVER['REQUEST_SCHEME'] .'://'. $_SERVER['SERVER_NAME'] .':'. $_SERVER['SERVER_PORT']. $_SERVER['REQUEST_URI']);
 ?>
